2ccee4b978d01f40e8cef7f8cdd33bc2041af4a1
Mathias Aebersold <mathias.aebersold@gmail.com>
2022-04-20 08:07:15 +0200

    Add speciality and add to vet. Closes #26

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Speciality.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Speciality extends BaseEntity {
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    private String description;
+}
